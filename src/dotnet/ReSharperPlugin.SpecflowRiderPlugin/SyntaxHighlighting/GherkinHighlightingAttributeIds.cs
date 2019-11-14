159a73e3a97ad1b372f342b0a89e1bdb53add38d
Valentin Livkin <104102+Settler@users.noreply.github.com>
2019-11-14 19:09:38 +0300

    Implemented most of highlighting logic (#10)

--- src/dotnet/ReSharperPlugin.SpecflowRiderPlugin/SyntaxHighlighting/GherkinHighlightingAttributeIds.cs
+++ src/dotnet/ReSharperPlugin.SpecflowRiderPlugin/SyntaxHighlighting/GherkinHighlightingAttributeIds.cs
@@ -6,33 +6,48 @@ using ReSharperPlugin.SpecflowRiderPlugin.SyntaxHighlighting;
     RegisterHighlighterGroup(GherkinHighlightingAttributeIds.GroupID, "SpecFlow", HighlighterGroupPriority.LANGUAGE_SETTINGS,
         RiderNamesProviderType = typeof(SpecFlowHighlighterNamesProvider)),
 
-//    RegisterHighlighter(GherkinHighlightingAttributeIds.NUMBER,
-//        GroupId = GherkinHighlightingAttributeIds.GroupID,
-//        EffectType = EffectType.TEXT,
-//        FallbackAttributeId = IdeaHighlightingAttributeIds.NUMBER,
-//        Layer = HighlighterLayer.SYNTAX),
     RegisterHighlighter(GherkinHighlightingAttributeIds.KEYWORD,
         GroupId = GherkinHighlightingAttributeIds.GroupID,
         EffectType = EffectType.TEXT,
         FallbackAttributeId = IdeaHighlightingAttributeIds.KEYWORD,
         Layer = HighlighterLayer.SYNTAX),
-    RegisterHighlighter(GherkinHighlightingAttributeIds.STRING,
+    RegisterHighlighter(GherkinHighlightingAttributeIds.TEXT,
         GroupId = GherkinHighlightingAttributeIds.GroupID,
         EffectType = EffectType.TEXT,
-        FallbackAttributeId = IdeaHighlightingAttributeIds.STRING,
+        FallbackAttributeId = IdeaHighlightingAttributeIds.TEXT,
         Layer = HighlighterLayer.SYNTAX),
     RegisterHighlighter(GherkinHighlightingAttributeIds.LINE_COMMENT,
         GroupId = GherkinHighlightingAttributeIds.GroupID,
         EffectType = EffectType.TEXT,
-        FallbackAttributeId = IdeaHighlightingAttributeIds.LINE_COMMENT,
-        Layer = HighlighterLayer.SYNTAX),
-    RegisterHighlighter(GherkinHighlightingAttributeIds.BLOCK_COMMENT,
-        GroupId = GherkinHighlightingAttributeIds.GroupID,
-        EffectType = EffectType.TEXT,
-        FallbackAttributeId = IdeaHighlightingAttributeIds.BLOCK_COMMENT,
+        FallbackAttributeId = IdeaHighlightingAttributeIds.DOC_COMMENT,
         Layer = HighlighterLayer.SYNTAX),
     RegisterHighlighter(GherkinHighlightingAttributeIds.TAG,
         GroupId = GherkinHighlightingAttributeIds.GroupID,
+        FallbackAttributeId = IdeaHighlightingAttributeIds.METADATA,
+        Layer = HighlighterLayer.SYNTAX),
+    RegisterHighlighter(GherkinHighlightingAttributeIds.REGEXP_PARAMETER,
+        GroupId = GherkinHighlightingAttributeIds.GroupID,
+        FallbackAttributeId = IdeaHighlightingAttributeIds.PARAMETER,
+        Layer = HighlighterLayer.SYNTAX),
+    RegisterHighlighter(GherkinHighlightingAttributeIds.TABLE_CELL,
+        GroupId = GherkinHighlightingAttributeIds.GroupID,
+        FallbackAttributeId = GherkinHighlightingAttributeIds.REGEXP_PARAMETER,
+        Layer = HighlighterLayer.SYNTAX),
+    RegisterHighlighter(GherkinHighlightingAttributeIds.OUTLINE_PARAMETER_SUBSTITUTION,
+        GroupId = GherkinHighlightingAttributeIds.GroupID,
+        FallbackAttributeId = IdeaHighlightingAttributeIds.INSTANCE_FIELD,
+        Layer = HighlighterLayer.SYNTAX),
+    RegisterHighlighter(GherkinHighlightingAttributeIds.TABLE_HEADER_CELL,
+        GroupId = GherkinHighlightingAttributeIds.GroupID,
+        FallbackAttributeId = GherkinHighlightingAttributeIds.OUTLINE_PARAMETER_SUBSTITUTION,
+        Layer = HighlighterLayer.SYNTAX),
+    RegisterHighlighter(GherkinHighlightingAttributeIds.PIPE,
+        GroupId = GherkinHighlightingAttributeIds.GroupID,
+        FallbackAttributeId = GherkinHighlightingAttributeIds.KEYWORD,
+        Layer = HighlighterLayer.SYNTAX),
+    RegisterHighlighter(GherkinHighlightingAttributeIds.PYSTRING,
+        GroupId = GherkinHighlightingAttributeIds.GroupID,
+        FallbackAttributeId = IdeaHighlightingAttributeIds.STRING,
         Layer = HighlighterLayer.SYNTAX)
 ]
 
@@ -40,14 +55,19 @@ namespace ReSharperPlugin.SpecflowRiderPlugin.SyntaxHighlighting
 {
     public class GherkinHighlightingAttributeIds
     {
+        // ReSharper disable InconsistentNaming
         public const string GroupID = "ReSharper SpecFlow Highlighters";
-
-//        public const string NUMBER = "ReSharper SpecFlow Number";
         public const string KEYWORD = "ReSharper SpecFlow Keyword";
-        public const string STRING = "ReSharper SpecFlow String";
+        public const string TEXT = "ReSharper SpecFlow Text";
         public const string LINE_COMMENT = "ReSharper SpecFlow Line Comment";
-        public const string BLOCK_COMMENT = "ReSharper SpecFlow Block Comment";
         public const string TAG = "ReSharper SpecFlow Tag";
+        public const string PYSTRING = "ReSharper SpecFlow Multiline String";
+        public const string TABLE_CELL = "ReSharper SpecFlow Table Cell";
+        public const string TABLE_HEADER_CELL = "ReSharper SpecFlow Table Header Cell";
+        public const string PIPE = "ReSharper SpecFlow Table Pipe";
+        public const string REGEXP_PARAMETER = "ReSharper SpecFlow Step Parameter";
+        public const string OUTLINE_PARAMETER_SUBSTITUTION = "ReSharper SpecFlow Scenario Outline Parameter";
+        // ReSharper restore InconsistentNaming
     }
 
     public class SpecFlowHighlighterNamesProvider : PrefixBasedSettingsNamesProvider
--- src/dotnet/ReSharperPlugin.SpecflowRiderPlugin/SyntaxHighlighting/Rider/GherkinSyntaxHighlightingProcessor.cs
+++ src/dotnet/ReSharperPlugin.SpecflowRiderPlugin/SyntaxHighlighting/Rider/GherkinSyntaxHighlightingProcessor.cs
@@ -1,21 +1,49 @@
-using JetBrains.Diagnostics;
-using JetBrains.Rd.Impl;
 using JetBrains.ReSharper.Daemon.SyntaxHighlighting;
+using JetBrains.ReSharper.Feature.Services.Daemon;
 using JetBrains.ReSharper.Psi.Parsing;
+using JetBrains.ReSharper.Psi.Tree;
 using ReSharperPlugin.SpecflowRiderPlugin.Psi;
 
 namespace ReSharperPlugin.SpecflowRiderPlugin.SyntaxHighlighting.Rider
 {
     public class GherkinSyntaxHighlightingProcessor : SyntaxHighlightingProcessor
     {
+        public override bool InteriorShouldBeProcessed(ITreeNode element, IHighlightingConsumer context)
+        {
+            if (element.NodeType == GherkinNodeTypes.TABLE_CELL &&
+                element.Parent?.NodeType == GherkinNodeTypes.TABLE_HEADER_ROW)
+            {
+                var documentRange = element.GetDocumentRange();
+                if (documentRange.TextRange.IsEmpty)
+                    return false;
+
+                context.AddHighlighting(new ReSharperSyntaxHighlighting(GherkinHighlightingAttributeIds.TABLE_HEADER_CELL, null, documentRange));
+                return false;
+            }
+
+            return base.InteriorShouldBeProcessed(element, context);
+        }
+
         public override string GetAttributeId(TokenNodeType tokenType)
         {
             if (tokenType == GherkinTokenTypes.TAG)
-            {
-                Protocol.TraceLogger.Log(LoggingLevel.INFO, $"GherkinSyntaxHighlighting:GetAttributeId – {tokenType}");
                 return GherkinHighlightingAttributeIds.TAG;
-            }
             
+            if (tokenType == GherkinTokenTypes.STEP_PARAMETER_TEXT)
+                return GherkinHighlightingAttributeIds.OUTLINE_PARAMETER_SUBSTITUTION;
+
+            if (tokenType == GherkinTokenTypes.TEXT)
+                return GherkinHighlightingAttributeIds.TEXT;
+
+            if (tokenType == GherkinTokenTypes.TABLE_CELL)
+                return GherkinHighlightingAttributeIds.TABLE_CELL;
+
+            if (tokenType == GherkinTokenTypes.STEP_PARAMETER_TEXT) //TODO Detect regex parameter like: "param"
+                return GherkinHighlightingAttributeIds.REGEXP_PARAMETER;
+
+            if (tokenType == GherkinTokenTypes.PIPE)
+                return GherkinHighlightingAttributeIds.PIPE;
+
             return base.GetAttributeId(tokenType);
         }
 
@@ -29,8 +57,15 @@ namespace ReSharperPlugin.SpecflowRiderPlugin.SyntaxHighlighting.Rider
             return GherkinTokenTypes.KEYWORDS[tokenType];
         }
 
+        protected override bool IsString(TokenNodeType tokenType)
+        {
+            return tokenType == GherkinTokenTypes.PYSTRING;
+        }
+
         protected override string KeywordAttributeId => GherkinHighlightingAttributeIds.KEYWORD;
 
         protected override string LineCommentAttributeId => GherkinHighlightingAttributeIds.LINE_COMMENT;
+        
+        protected override string StringAttributeId => GherkinHighlightingAttributeIds.PYSTRING;
     }
 }
