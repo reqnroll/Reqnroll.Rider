f4531c2ae20463e3e39a17bd9ecbb08d0c890ea4
Alexandra Horváth-Fung <fung.alexandra@gmaill.com>
2021-03-05 11:23:38 +0100

    Add gherkin for syntax highlighting

---
+++ src/dotnet/ReSharperPlugin.SpecflowRiderPlugin.Tests/Utils/Steps/SyntaxHighlightingSteps.cs
@@ -0,0 +1,14 @@
+using TechTalk.SpecFlow;
+
+namespace ReSharperPlugin.SpecflowRiderPlugin.Tests.Utils.Steps
+{
+    [Binding]
+    public class SyntaxHighlightingSteps
+    {
+
+        [Given(@"this a step with a string (.+)")]
+        public void GivenThisAStepWithAString(string p1)
+        {
+        }
+    }
+}
