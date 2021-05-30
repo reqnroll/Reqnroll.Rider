b632cb4cd2c7ff4416f252f70c49718ed6e4881f
Socolin <provost.bertrand@gmail.com>
2021-05-30 14:44:00 -0400

    Improve how plugin archive (.zip) is generated in pipeline (avoid double .zip)

---
+++ scripts/Unpack-Distribution.ps1
@@ -0,0 +1,19 @@
+
+<#
+    .SYNOPSIS
+        Taken from: https://github.com/ForNeVeR/AvaloniaRider/blob/master/scripts/Unpack-Distribution.ps1
+        The purpose of this script is to unpack the compressed plugin artifact.
+        It is used during CI builds to generate the layout for uploading.
+    .PARAMETER DistributionsLocation
+        Path to the directory containing compressed plugin distribution.
+#>
+param (
+	[string] $DistributionsLocation = "$PSScriptRoot/../build/distributions"
+)
+
+Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
+
+$file = "$DistributionsLocation/specflowriderplugin-*.zip"
+
+Expand-Archive -Path $file -DestinationPath $DistributionsLocation/unpacked
