9eee485c3167c7a3026a962ccd86a699d9136814
Socolin <provost.bertrand@gmail.com>
2023-12-17 15:35:47 -0500

    Fix matching feature with tests when path contains +

--- src/dotnet/ReSharperPlugin.SpecflowRiderPlugin/UnitTestExplorers/NamespaceHelper.cs
+++ src/dotnet/ReSharperPlugin.SpecflowRiderPlugin/UnitTestExplorers/NamespaceHelper.cs
@@ -19,6 +19,7 @@ public static class NamespaceHelper
                 case '\'':
                 case '"':
                 case ' ':
+                case '+':
                 case '\t':
                     lastIsSeparator = true;
                     break;
