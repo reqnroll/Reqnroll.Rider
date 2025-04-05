bb4cea2c9183a21470f2b0649a0251e09ae0b25c
Socolin <provost.bertrand@gmail.com>
2025-04-05 16:22:17 -0400

    Adjust language injection in .feature to not include the indent. This fix error with xml doctype

--- src/rider/main/kotlin/com/jetbrains/rider/plugins/reqnrollriderplugin/injector/LanguageInjector.kt
+++ src/rider/main/kotlin/com/jetbrains/rider/plugins/reqnrollriderplugin/injector/LanguageInjector.kt
@@ -23,25 +23,32 @@ class LanguageInjector : MultiHostInjector {
         val host = context as PsiLanguageInjectionHost
         val hostText = host.text
         var newLineCharacterOffset = 0
-        while (newLineCharacterOffset < hostText.length && hostText[newLineCharacterOffset] != '\n') {
+        val newLineOffsets = ArrayList<Int>()
+        while (newLineCharacterOffset < hostText.length) {
+            if (hostText[newLineCharacterOffset] == '\n')
+                newLineOffsets.add(newLineCharacterOffset)
             newLineCharacterOffset++
         }
-        if (newLineCharacterOffset >= hostText.length) {
+        if (newLineOffsets.count() < 2) {
             return
         }
+        val firstNewLineOffset = newLineOffsets[0];
         val languageMarker =
-            StringUtil.trimTrailing(hostText.substring(GherkinLexer.PYSTRING_MARKER.length, newLineCharacterOffset))
+            StringUtil.trimTrailing(hostText.substring(GherkinLexer.PYSTRING_MARKER.length, firstNewLineOffset))
         val language: Language? = InjectorUtils.getLanguageByString(languageMarker)
+        val indent = context.prevSibling.text.trim('\n', '\r').length;
         if (language != null) {
-            val range = TextRange.create(newLineCharacterOffset, host.textLength - GherkinLexer.PYSTRING_MARKER.length)
+            val range = TextRange.create(firstNewLineOffset, host.textLength - GherkinLexer.PYSTRING_MARKER.length)
             if (!range.isEmpty) {
-                val extensionPoint = GherkinInjectorExtensionPoint.EP_NAME.extensionList.stream().findFirst()
-                val prefix = extensionPoint.map { ep: GherkinInjectorExtensionPoint -> ep.getPrefix(languageMarker) }
-                    .orElse(null)
-                val suffix = extensionPoint.map { ep: GherkinInjectorExtensionPoint -> ep.getSuffix(languageMarker) }
-                    .orElse(null)
                 registrar.startInjecting(language)
-                registrar.addPlace(prefix, suffix, host, range)
+                for (i in 0 until newLineOffsets.size - 1) {
+                    val startLineOffset = newLineOffsets[i] + indent + 1;
+                    val endLineOffset = newLineOffsets[i + 1];
+                    if (startLineOffset < endLineOffset) {
+                        val lineRange = TextRange.create(startLineOffset, endLineOffset)
+                        registrar.addPlace(null, null, host, lineRange)
+                    }
+                }
                 registrar.doneInjecting()
             }
         }
