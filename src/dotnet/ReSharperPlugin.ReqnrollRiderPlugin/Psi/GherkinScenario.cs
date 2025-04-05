4d0897e2a9737e60861324dc858fe5cfbadcf509
Socolin <provost.bertrand@gmail.com>
2025-04-05 17:36:22 -0400

    Do not show `Missing Title` error for Background: Fix #43

--- src/dotnet/ReSharperPlugin.ReqnrollRiderPlugin/Psi/GherkinScenario.cs
+++ src/dotnet/ReSharperPlugin.ReqnrollRiderPlugin/Psi/GherkinScenario.cs
@@ -3,62 +3,67 @@ using System.Linq;
 using JetBrains.Annotations;
 using JetBrains.ReSharper.Psi.Tree;
 
-namespace ReSharperPlugin.ReqnrollRiderPlugin.Psi
+namespace ReSharperPlugin.ReqnrollRiderPlugin.Psi;
+
+public interface IGherkinScenario : ITreeNode
 {
-    public interface IGherkinScenario : ITreeNode
+    bool IsBackground();
+    string GetScenarioText();
+    IEnumerable<GherkinStep> GetSteps();
+}
+
+public class GherkinScenario() : GherkinElement(GherkinNodeTypes.SCENARIO), IGherkinScenario
+{
+    [CanBeNull] private IList<string> _cachedTags;
+
+    public bool IsBackground()
     {
-        bool IsBackground();
-        string GetScenarioText();
-        IEnumerable<GherkinStep> GetSteps();
+        var firstNonCommentAndWhitespaceChild = FirstChild;
+        while (firstNonCommentAndWhitespaceChild != null)
+        {
+            if (firstNonCommentAndWhitespaceChild is not GherkinComment
+                && !firstNonCommentAndWhitespaceChild.IsWhitespaceToken())
+                break;
+
+            firstNonCommentAndWhitespaceChild = firstNonCommentAndWhitespaceChild.NextSibling;
+        }
+
+        return firstNonCommentAndWhitespaceChild?.NodeType == GherkinTokenTypes.BACKGROUND_KEYWORD;
     }
 
-    public class GherkinScenario : GherkinElement, IGherkinScenario
+    public string GetScenarioText()
     {
-        [CanBeNull] private IList<string> _cachedTags;
+        return this.FindChild<GherkinToken>(o => o.NodeType == GherkinTokenTypes.TEXT)?.GetText();
+    }
 
-        public GherkinScenario() : base(GherkinNodeTypes.SCENARIO)
+    public IList<string> GetTags()
+    {
+        return _cachedTags ??= ListTags().ToList();
+    }
+
+    private IEnumerable<string> ListTags()
+    {
+        var node = FirstChild;
+        while (node != null)
         {
+            if (node is GherkinTag tag)
+                yield return tag.GetTagText();
+            else if (!node.IsWhitespaceToken() && node is not GherkinLanguageComment)
+                break;
+            node = node.NextSibling;
         }
+    }
 
-        public bool IsBackground()
-        {
-            return FirstChild?.NodeType == GherkinTokenTypes.BACKGROUND_KEYWORD;
-        }
+    public IEnumerable<GherkinStep> GetSteps()
+    {
+        return this.Children<GherkinStep>();
+    }
 
-        public string GetScenarioText()
-        {
-            return this.FindChild<GherkinToken>(o => o.NodeType == GherkinTokenTypes.TEXT)?.GetText();
-        }
+    public override string ToString()
+    {
+        if (IsBackground())
+            return "GherkinScenario(Background):";
 
-        public IList<string> GetTags()
-        {
-            return _cachedTags ??= ListTags().ToList();
-        }
-
-        private IEnumerable<string> ListTags()
-        {
-            var node = FirstChild;
-            while (node != null)
-            {
-                if (node is GherkinTag tag)
-                    yield return tag.GetTagText();
-                else if (!node.IsWhitespaceToken() && node is not GherkinLanguageComment)
-                    break;
-                node = node.NextSibling;
-            }
-        }
-
-        public IEnumerable<GherkinStep> GetSteps()
-        {
-            return this.Children<GherkinStep>();
-        }
-
-        public override string ToString()
-        {
-            if (IsBackground())
-                return "GherkinScenario(Background):";
-
-            return $"GherkinScenario: {GetScenarioText()}";
-        }
+        return $"GherkinScenario: {GetScenarioText()}";
     }
 }
