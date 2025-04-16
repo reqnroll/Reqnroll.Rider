4f0b85f917fb5d2744c9f5ef5777dff7cd7563b6
Socolin <provost.bertrand@gmail.com>
2025-04-16 19:46:06 -0400

    Temp: Fix completion in 2025.1. Definitive fix will come later (Waiting after 2025.1.1)

--- src/rider/main/kotlin/com/jetbrains/rider/plugins/reqnrollriderplugin/psi/completion/GherkinCompletionContributor.kt
+++ src/rider/main/kotlin/com/jetbrains/rider/plugins/reqnrollriderplugin/psi/completion/GherkinCompletionContributor.kt
@@ -3,12 +3,15 @@ package com.jetbrains.rider.plugins.reqnrollriderplugin.psi.completion
 import com.intellij.codeInsight.completion.CompletionParameters
 import com.intellij.codeInsight.completion.CompletionProvider
 import com.jetbrains.rider.completion.ProtocolCompletionContributor
+import com.jetbrains.rider.completion.patchEngine.RiderPatchEngineCompletionContributor
 
-class GherkinCompletionContributor : ProtocolCompletionContributor() {
+class GherkinCompletionContributor : RiderPatchEngineCompletionContributor() {
 
+/*
     override fun createCompletionProvider(): CompletionProvider<CompletionParameters> {
         return GherkinCompletionProvider(this);
     }
+*/
 
 
 }
