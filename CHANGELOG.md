843250208bb4f0b0ee5850cf109154d5ac59eda4
Socolin <provost.bertrand@gmail.com>
2025-11-18 20:04:13 -0400

    Fix step completion edge cases

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,9 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## 2025.3.1
+- Fix step completion not always working as expected after a space or at the end of a line
+
 ## 2025.3.0
 - Support for Rider 2025.3
 
--- src/rider/main/kotlin/com/jetbrains/rider/plugins/reqnrollriderplugin/psi/completion/GherkinCompletionHelper.kt
+++ src/rider/main/kotlin/com/jetbrains/rider/plugins/reqnrollriderplugin/psi/completion/GherkinCompletionHelper.kt
@@ -6,6 +6,8 @@ import com.intellij.patterns.ElementPattern
 import com.intellij.patterns.PatternCondition
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiWhiteSpace
+import com.intellij.psi.util.lastLeaf
+import com.intellij.refactoring.suggested.extend
 import com.intellij.util.ProcessingContext
 import com.jetbrains.rider.completion.CustomCharPattern
 import com.jetbrains.rider.completion.ICompletionHelper
@@ -20,15 +22,23 @@ class GherkinCompletionHelper : ICompletionHelper {
     ): ElementPattern<Char>? {
         var currentOffset = completionOffset
         var psiElement = element.findElementAt(completionOffset)
-        if (psiElement is PsiWhiteSpace)
-            if (psiElement.parent !is GherkinStep)
+        var addSpace = false;
+        if (psiElement is PsiWhiteSpace) {
+            if (psiElement.text.startsWith(' '))
+                addSpace = true;
+            if (psiElement.prevSibling.lastLeaf().parent is GherkinStep)
+                psiElement = psiElement.prevSibling.lastLeaf()
+            else if (psiElement.parent !is GherkinStep)
                 psiElement = psiElement.prevSibling?.lastChild
             else
                 psiElement = psiElement.prevSibling
+        }
         if (psiElement?.parent !is GherkinStep) {
             return null
         }
-        val completionTextRange = psiElement.textRange?.intersection(TextRange(0, completionOffset))
+        var completionTextRange = psiElement.textRange?.intersection(TextRange(0, completionOffset))
+        if (addSpace && (psiElement.parent as GherkinStep).name != "")
+            completionTextRange = completionTextRange?.grown(1)
 
         return CustomCharPattern.customCharacter().with(object : PatternCondition<Char>("gherkinIdentifierPart") {
             override fun accepts(character: Char, context: ProcessingContext): Boolean =
@@ -44,15 +54,23 @@ class GherkinCompletionHelper : ICompletionHelper {
     ): ElementPattern<Char>? {
         var currentOffset = completionOffset
         var psiElement = element.findElementAt(completionOffset)
-        if (psiElement is PsiWhiteSpace)
-            if (psiElement.parent !is GherkinStep)
+        var addSpace = false;
+        if (psiElement is PsiWhiteSpace) {
+            if (psiElement.text.startsWith(' '))
+                addSpace = true;
+            if (psiElement.prevSibling.lastLeaf().parent is GherkinStep)
+                psiElement = psiElement.prevSibling.lastLeaf()
+            else if (psiElement.parent !is GherkinStep)
                 psiElement = psiElement.prevSibling?.lastChild
             else
                 psiElement = psiElement.prevSibling
+        }
         if (psiElement?.parent !is GherkinStep) {
             return null
         }
-        val completionTextRange = psiElement.textRange?.intersection(TextRange(0, completionOffset))
+        var completionTextRange = psiElement.textRange?.intersection(TextRange(0, completionOffset))
+        if (addSpace && (psiElement.parent as GherkinStep).name != "")
+            completionTextRange = completionTextRange?.grown(1)
 
         return CustomCharPattern.customCharacter().with(object : PatternCondition<Char>("gherkinIdentifierStart") {
             override fun accepts(character: Char, context: ProcessingContext): Boolean =
